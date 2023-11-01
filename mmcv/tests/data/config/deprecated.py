b8744aab0c6cda2c973e4380d2f9facf91a478e9
thb1314 <13953062665@163.com>
2023-11-01 23:22:36 +0800

    mmcv

---
+++ mmcv/tests/data/config/deprecated.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+_base_ = './expected.py'
+
+_deprecation_ = dict(
+    expected='tests/data/config/expected.py',
+    reference='https://github.com/open-mmlab/mmcv/pull/1275')
